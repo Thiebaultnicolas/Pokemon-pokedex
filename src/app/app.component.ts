5d1df2200a871ab4ab90876b01475ca2e216fbc1
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-29 00:42:17 +0200

    explication mauvaise méthode constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemons[1] }}!</h1>`     
+  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`     
 })
-export class AppComponent {
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+/*
+  constructor() {
+    this.pokemonList =  []; 
+  }
+*/
+
+    /* ❌ ❌ ❌ on peut appercevoir des gens faire ce construstor (au dessus) pour initialiser
+    leurs propriété ici dans le constructeur , il est fortement deconseiller 
+    d'utiliser cette facon de faire , il est recommandé de garder la logique
+    du composant en dehors du construtor surtout quand on commencera a 
+    récuperer les données d'un serveur distant et plus important le constucteur 
+    n'ai pas fait pour ceci la methode mise en place par angular pour declarer 
+    des propriétés et les liées avec notre template ca se passe dans ngOnInit 
+    ci dessous.*/
+    
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
 }
