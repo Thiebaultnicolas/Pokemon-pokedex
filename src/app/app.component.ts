de24f2d486391bb1aeb03067aea83e739767b883
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-29 00:51:08 +0200

    gérer intéractionsavec utilisateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,10 @@ export class AppComponent implements OnInit {
     
   ngOnInit() {
     console.table(this.pokemonList);
+    this.selectPokemon('Bulbizarre');
+  }
+
+  selectPokemon(pokemonName: string) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
   }
 }
