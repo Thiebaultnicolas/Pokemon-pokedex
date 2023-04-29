5b52ca29d3c4135bc9b68af5b5ffd99363c0b73a
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-29 15:49:09 +0200

    actions utilisateur (HostListener)

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]'
@@ -8,6 +8,14 @@ export class BorderCardDirective {
   constructor(private el:ElementRef) {
      this.settHeight(180);
      this.setBorder('#f5f5f5');
+   }
+
+   @HostListener('mouseenter') onMouseEnter () {
+     this.setBorder('#009688');
+   }
+
+   @HostListener('mouseleave') onMouseLeave () {
+    this.setBorder('#f5f5f5');
    }
 
   settHeight(height: number) {
