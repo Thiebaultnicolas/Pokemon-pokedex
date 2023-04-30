286adcb91f7d9d0dc5b8406c30af4ce331a3230a
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-30 15:41:08 +0200

    Dynamiser le composant détail d'un pokémon

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -3,14 +3,30 @@ la commande ( ng generate component detail-pokemon --inline-template=false )
  l'option inline template a false permet d'avoir le template du composant
  a part ce qui permet d'avoir deux fichier le ts et le html*/
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
+
+  pokemonList: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor (private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+
+    if (pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
+  }
 
 }
