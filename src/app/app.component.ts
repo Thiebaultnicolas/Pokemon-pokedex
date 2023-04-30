018149d4484409dec598e4f1fd759710fd11ae2d
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-30 15:21:17 +0200

    modification composant liste de pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'     
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-/*
-  constructor() {
-    this.pokemonList =  []; 
-  }
-*/
-
-    /* ❌ ❌ ❌ on peut appercevoir des gens faire ce construstor (au dessus) pour initialiser
-    leurs propriété ici dans le constructeur , il est fortement deconseiller 
-    d'utiliser cette facon de faire , il est recommandé de garder la logique
-    du composant en dehors du construtor surtout quand on commencera a 
-    récuperer les données d'un serveur distant et plus important le constucteur 
-    n'ai pas fait pour ceci la methode mise en place par angular pour declarer 
-    des propriétés et les liées avec notre template ca se passe dans ngOnInit 
-    ci dessous.*/
-    
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string ) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id === parseInt(pokemonId));
-   
-    if(pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-   
-  }
+export class AppComponent { 
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -4,13 +4,15 @@ la commande ( ng generate component list-pokemon --inline-template=false )
  a part ce qui permet d'avoir deux fichier le ts et le html*/
 
 import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styles: [
-  ]
+ 
 })
-export class ListPokemonComponent {
 
+export class ListPokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS;
 }
