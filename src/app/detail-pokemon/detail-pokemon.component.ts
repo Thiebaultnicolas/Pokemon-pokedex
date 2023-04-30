29683cfac28bc3631a49a91477b3a48d8be3b2e5
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-04-30 14:55:29 +0200

    création de routes

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,7 +1,7 @@
 /* pour mettre en place les deux nouveau composant j'ai utiliser depuis le terminal 
-la commande ( ng generate component list-pokemon --inline-template=false ) 
+la commande ( ng generate component detail-pokemon --inline-template=false ) 
  l'option inline template a false permet d'avoir le template du composant
- a part ce qui permet d'avoir deux fichier  le ts et le html*/
+ a part ce qui permet d'avoir deux fichier le ts et le html*/
 
 import { Component } from '@angular/core';
 
