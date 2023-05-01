9a763bd93d6f0b4e44e2cf5a370e811dbd2365f2
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-01 13:47:19 +0200

    gestion des erreurs

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -12,22 +12,24 @@ export class PokemonService {
 
   getPokemonList(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>('api/pokemons').pipe(
-      tap((pokemonList) => console.table(pokemonList)),
-      catchError((error) => {
-      console.log(error);
-      return of([]);
-    })
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
     );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
     return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
-      tap((pokemon) => console.log(pokemon)),
-      catchError((error) => {
-      console.log(error);
-      return of(undefined);
-    })
-    )
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
+
+  private log(response: Pokemon[]|Pokemon|undefined) {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of (errorValue);
   }
 
   getpokemonTypeList(): string[] {
