ea2f4fdc5a73d7aa0bb784cbaed05b6135515960
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-02 00:11:09 +0200

    message d'aide pour ajout pokemon

--- src/app/pokemon/add-pokemon/add-pokemon.component.ts
+++ src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -5,6 +5,7 @@ import { Pokemon } from '../pokemon';
   selector: 'app-add-pokemon',
   template: `
     <h2 class="center">Ajouter un pokémon</h2>
+    <h5 class="center">(Dans l'img xxx par 056/054/270 etc...)</h5>
     <app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
   `
 })
