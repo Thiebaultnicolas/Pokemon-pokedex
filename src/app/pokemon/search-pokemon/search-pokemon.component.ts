137ed20601a5dc4d91a15db05777bbeae6a37084
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-01 17:42:31 +0200

    construction observable personalisé

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -14,14 +15,22 @@ export class SearchPokemonComponent implements OnInit {
   pokemons$:  Observable<Pokemon[]>;
 
  constructor (
-  private router: Router
+  private router: Router,
+  private pokemonService: PokemonService,
 
-  ) {
-
-   }
+  ) { }
 
  ngOnInit(): void {
-   
+   this.pokemons$ = this.searchTerms.pipe(
+     //{..."a"."ab"."abz".."ab"..."abc"......}
+     debounceTime(300),
+     //{......"ab"..."ab"..."abc"......}
+     distinctUntilChanged(),
+     //{......"ab".........."abc"......}
+     switchMap((term) => this.pokemonService.searchPokemonList(term))// concatMap / mergeMap / switchMap (dans la plus-part des cas utiliser switchMap)
+     //{.....pokemonList(ab)........pokemonList(abc)......}
+     
+   );
  }
 
  search(term: string) {
